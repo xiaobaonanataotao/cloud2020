a9c09da92d3614c5592519fe49fbe29d9a9dbe86
xiaobaonanataotao <1062180489@qq.com>
2020-05-09 13:56:51 +0800

    Consul安装测试

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,46 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class.getName());// slf4j日志记录器
+
+    @Resource
+    private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
+    @PostMapping(value = "/payment/create")
+    public CommonResult<Payment> create(@RequestBody  Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果："+result);
+        log.info("*****插入结果插入结果插入结果插入结果："+result);
+        if (result>0){  //成功
+            return new CommonResult(200,"插入数据库成功 serverPort:"+serverPort,result);
+        }else {
+            return new CommonResult(444,"插入数据库失败 serverPort:"+serverPort,null);
+        }
+    }
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@RequestBody @PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果："+payment);
+        if (payment!=null){  //说明有数据，能查询成功
+            return new CommonResult(200,"查询成功 serverPort:"+serverPort,payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
+        }
+    }
+}
+
+
