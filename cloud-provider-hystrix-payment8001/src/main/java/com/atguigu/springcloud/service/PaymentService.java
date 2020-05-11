9534fb753f9cdc2b6ffeabd21cb9183ff7405561
xiaobaonanataotao <1062180489@qq.com>
2020-05-11 19:59:09 +0800

    提交

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -13,12 +15,18 @@ public class PaymentService {
     }
 
     //失败
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")  //3秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 3;
+        int timeNumber = 5;
         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
         return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
     }
-
+    //兜底方法
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
+    }
 }
 
 
