ded7b955a1761a394353b1f623521ba306d9dca0
xiaobaonanataotao <1062180489@qq.com>
2020-05-13 19:29:01 +0800

    eureka7002

--- cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaMain7002 {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7002.class,args);
     }
