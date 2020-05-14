c47218f94a8b050203a007883af620727705ac6d
xiaobaonanataotao <1062180489@qq.com>
2020-05-14 14:55:14 +0800

    sc

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -16,16 +16,22 @@ public class PaymentService {
 
     //失败
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")  //3秒钟以内就是正常的业务逻辑
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")  //3秒钟以内就是正常的业务逻辑
     })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber = 5;
-        try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
-        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
+        int timeNumber = 3000;
+        int i=10/0;
+        try {
+            Thread.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"OKOKOKOOKOKOK"+" 耗时(秒)"+timeNumber;
     }
     //兜底方法
     public String paymentInfo_TimeOutHandler(Integer id){
-        return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
+        return "线程池："+Thread.currentThread().getName()+"   系统出错  ,id：  "+id+"\t"+"哭了哇呜";
+//        return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
     }
 }
 
