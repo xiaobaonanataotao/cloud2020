09b453e4dc2f61937f20b7f4d65f8fd538c3e2e1
xiaobaonanataotao <1062180489@qq.com>
2020-05-31 22:38:13 +0800

    nacos

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMain.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain.class,args);
+    }
+
+}
