c11e80e249a4b7d879813464e184be35f9c1a7b8
xiaobaonanataotao <1062180489@qq.com>
2020-06-04 17:48:00 +0800

    83

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -24,13 +25,14 @@ public class FlowLimitController
     @GetMapping("/testB")
     public String testB()
     {
+
+        System.out.println("testB 异常比例");
         return "------testB";
     }
     @GetMapping("/testD")
     public String testD()
     {
 //        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-//        log.info("testD 测试RT");
 
         System.out.println("testD 异常比例");
         int age = 10/0;
